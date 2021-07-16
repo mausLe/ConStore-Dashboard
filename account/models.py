695ce0e66daf30c850b30700792991553b1af0c9
mausLe <anhlt.cse@gmail.com>
2021-07-17 03:20:55 +0700

    Update models

--- account/models.py
+++ account/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Watchlist(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=30, null=True)
+    phone = models.CharField(max_length=30, null=True)
+    time = models.DateTimeField(auto_now_add=True, null=True)
+    type = models.IntegerField(null=False)
+
+    def __str__(self):
+        return self.id
