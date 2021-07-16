cdb369a53966cdd2373482e5142394e612ae4fed
mausLe <anhlt.cse@gmail.com>
2021-07-17 02:14:38 +0700

    Update 1

--- account/views.py
+++ account/views.py
@@ -2,12 +2,21 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+from PIL import Image
+# img = Image.open(r"C:\Users\Maus\Pictures\ProfilewFrame.jpg")
+
+img = Image.open(r"account\static\img\img02.jpg")
 
 
 
 
 def home(request):
-    return render(request, "account/dashboard.html")
+    context = []
+
+    person = {"name": "Lê Tuấn Anh", "id":17520237, "image":img}
+    context.append(person)
+
+    return render(request, "account/dashboard.html", {"context":context})
 
 def sync(request): 
     return HttpResponse("Sync Page")
